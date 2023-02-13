11d52e38d989ea7c16383f1c1e80dcb079aa5545
josoya <thdi4564@nate.com>
2023-02-13 12:08:32 +0900

    영속성 컨테스트 마지막

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args){
@@ -13,8 +12,10 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+
 //            Member findMember = em.find(Member.class, 1L);
 //            findMember.setName("HelloJPA");
+
 //            // 비영속
 //            Member member = new Member();
 //            member.setId(101L);
@@ -24,15 +25,27 @@ public class JpaMain {
 //            System.out.println("==== BEFORE =====");
 //            em.persist(member);
 //            // 준영속(영속성 컨테스트 지우기)
-////            em.detach(member);
+//            em.detach(member);
 //            System.out.println("==== AFTER =====");
 
+//            // 쓰기 지연 batch - persistence.xml 파일에 설정해야함. <property name="hibernate.jdbc.batch_size" value="10"/>
+//            Member m1 = new Member(15L, "SS1");
+//            Member m2 = new Member(16L, "SS2");
+//            Member m3 = new Member(17L, "SS3");
+//            Member m4 = new Member(18L, "SS4");
+//            em.persist(m1);
+//            em.persist(m2);
+//            em.persist(m3);
+//            em.persist(m4);
 
+            // 변경 감지 (dirty checking) - member.update()가 존재하지 않다. 영속성 컨텍스트가 처리함.
+//            Member member = em.find(Member.class, 150L);
+//            member.setName("AAAAAA");
 
-//            변경 감지 (dirty checking) - member.update()가 존재하지 않다. 영속성 컨텍스트가 처리함.
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAAA");
-            em.detach(member);
+//            em.detach(member);
+
+            // 직접 디비에 커밋 시킴.
+//            em.flush();
 
             // 영속성 컨텍스트 초기화
 //            em.clear();
@@ -50,8 +63,10 @@ public class JpaMain {
 //            Member findMember2 = em.find(Member.class, 101L);
 
 //            System.out.println("result = " + (findMember1 == findMember2));
+
             //db에 쿼리 적용
             tx.commit();
+
         } catch (Exception e) {
             tx.rollback();
         }finally {
