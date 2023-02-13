e7ae318f0da35f30f8dd08e63c6f438e8f81d593
josoya <thdi4564@nate.com>
2023-02-13 14:16:22 +0900

    매핑 어노테이션

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,24 +1,59 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+// db 테이블명에 맞춰서 사용
+//@Table(name = "MBR")
 public class Member {
 
     public Member() {
 
     }
 
-    public Member(Long id, String name) {
+    public Member(Long id, String username, Integer age, RoleType roleType, Date createdDate, Date lastModifiedDate, String description) {
         this.id = id;
-        this.name = name;
+        this.username = username;
+        this.age = age;
+        this.roleType = roleType;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", age=" + age +
+                ", roleType=" + roleType +
+                ", createdDate=" + createdDate +
+                ", lastModifiedDate=" + lastModifiedDate +
+                ", description='" + description + '\'' +
+                '}';
     }
 
     @Id
     private Long id;
 
-    private String name;
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+    // enum 타입 지정 어노테이션 - enum 에는 타입이 없어서 사용
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+    // 날짜 어노테이션
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    // 큰 컨텐츠를 넣을 때 사용
+    @Lob
+    private String description;
 
     public Long getId() {
         return id;
@@ -28,11 +63,51 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
